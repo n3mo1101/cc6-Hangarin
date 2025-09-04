d271be05e79bceefc9b7d3da07267ce8b14dc290
n3mo1101 <rd.cruz1101@gmail.com>
2025-09-04 09:50:56 +0800

    loaded initial data

---
+++ projectsite/task_manager/management/commands/create_initial_data.py
@@ -0,0 +1,56 @@
+from django.core.management.base import BaseCommand
+from faker import Faker
+from django.utils import timezone
+from task_manager.models import Priority, Category, Task, SubTask, Note
+
+class Command(BaseCommand):
+    help = 'Create initial data for the application'    
+        
+    def handle(self, *args, **kwargs):
+        self.create_task(15)
+        self.create_subtask(30)
+        self.create_note(15)
+
+
+    def create_task(self, count):
+        fake = Faker() 
+
+        for _ in range(count):
+            Task.objects.create(
+                title=fake.sentence(nb_words=5).title(),
+                description=fake.paragraph(nb_sentences=3),
+                deadline=timezone.make_aware(fake.date_time_this_month()),
+                status=fake.random_element(),
+                category=Category.objects.order_by('?').first(),
+                priority=Priority.objects.order_by('?').first(),
+            )
+
+        self.stdout.write(self.style.SUCCESS(
+        'Initial data for task created successfully.'))
+
+    
+    def create_subtask(self, count):
+        fake = Faker()
+        
+        for _ in range(count):
+            SubTask.objects.create(
+                parent_task=Task.objects.order_by('?').first(),
+                title=fake.sentence(nb_words=5).title(),
+                status=fake.random_element(),
+            )
+
+        self.stdout.write(self.style.SUCCESS(
+        'Initial data for subtask created successfully.'))
+
+    
+    def create_note(self, count):
+        fake = Faker()
+
+        for _ in range(count):
+            Note.objects.create(
+                task=Task.objects.order_by('?').first(),
+                content=fake.paragraph(nb_sentences=3),
+            )
+        
+        self.stdout.write(self.style.SUCCESS(
+        'Initial data for note created successfully.'))
