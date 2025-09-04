647913f7be1f5324a4ffac91b6be9a6ba707049e
n3mo1101 <rd.cruz1101@gmail.com>
2025-09-04 08:57:56 +0800

    models and admin working properly

--- projectsite/task_manager/admin.py
+++ projectsite/task_manager/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
+from .models import Priority, Category, Task, SubTask, Note
 
 # Register your models here.
+admin.site.register(Priority)
+admin.site.register(Category)
+admin.site.register(Task)
+admin.site.register(SubTask)
+admin.site.register(Note)
