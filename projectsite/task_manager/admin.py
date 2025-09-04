1412d8b7bc76be8af62086e9a6fb9255f34f0cfd
n3mo1101 <rd.cruz1101@gmail.com>
2025-09-04 10:26:07 +0800

    refactored admin.py

--- projectsite/task_manager/admin.py
+++ projectsite/task_manager/admin.py
@@ -2,8 +2,41 @@ from django.contrib import admin
 from .models import Priority, Category, Task, SubTask, Note
 
 # Register your models here.
-admin.site.register(Priority)
-admin.site.register(Category)
-admin.site.register(Task)
-admin.site.register(SubTask)
-admin.site.register(Note)
+@admin.register(Priority)
+class PriorityAdmin(admin.ModelAdmin):
+    list_display = ("name",)
+    search_fields = ("name",)
+
+
+@admin.register(Category)
+class CategoryAdmin(admin.ModelAdmin):
+    list_display = ("name",)
+    search_fields = ("name",)
+
+
+@admin.register(Task)
+class TaskAdmin(admin.ModelAdmin):
+    list_display = ("title", "status", "deadline", "priority", "category")
+    list_filter = ("status", "priority", "category",)
+    search_fields = ("title", "description",)
+
+
+@admin.register(SubTask)
+class SubTaskAdmin(admin.ModelAdmin):
+    list_display = ("title", "status", "get_parent_task")
+    list_filter = ("status",)
+    search_fields = ("title",)
+
+    def get_parent_task(self, obj):
+        try:
+            parent_task = Task.objects.get(id=obj.id)
+            return parent_task.title
+        except Task.DoesNotExist:
+            return None
+    
+
+@admin.register(Note)
+class NoteAdmin(admin.ModelAdmin):
+    list_display = ("task", "content", "created_at")
+    list_filter = ("created_at",)
+    search_fields = ("content",)
